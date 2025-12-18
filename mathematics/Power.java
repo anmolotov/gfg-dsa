687e4d923f6e2445e3173862370b5175fee94bd8
Anmol Singh <anmolotov05@gmail.com>
2025-12-19 01:27:44 +0530

    add iterative power

--- mathematics/Power.java
+++ mathematics/Power.java
@@ -17,7 +17,22 @@ public class Power {
     }
   }
 
+  public static int iterativePow(int base, int power) {
+    int res = 1;
+
+    while (power > 0) {
+      if (power % 2 != 0) {
+        res = res * base;
+      }
+
+      base = base * base;
+      power = power / 2;
+    }
+    return res;
+  }
+
   public static void main(String[] args) {
     System.out.println(pow(2, 3));
+    System.out.println(iterativePow(2, 3));
   }
 }
