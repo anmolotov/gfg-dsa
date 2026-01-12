0f38cef985915ac538db24eb19a6f296f087cb08
Anmol Singh <anmolotov05@gmail.com>
2026-01-12 21:49:00 +0530

    print n to 1

---
+++ recursion/NtoOne.java
@@ -0,0 +1,14 @@
+public class NtoOne {
+  static void print(int n) {
+    if (n == 0) {
+      return;
+    }
+    System.out.print(n + " ");
+
+    print(n - 1);
+  }
+
+  public static void main(String[] args) {
+    print(10);
+  }
+}
