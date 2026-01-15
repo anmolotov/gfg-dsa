a5b5eaaf23c12aad2c6156b06ddf13f8514ba32d
Anmol Singh <anmolotov05@gmail.com>
2026-01-15 10:10:38 +0530

    Left rotate the array by one

---
+++ array/LeftRotate.java
@@ -0,0 +1,21 @@
+public class LeftRotate {
+  static void rotate(int[] arr) {
+    int temp = arr[0];
+
+    for (int i = 0; i < arr.length - 1; i++) {
+      arr[i] = arr[i + 1];
+    }
+
+    arr[arr.length - 1] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 3, 4, 5 };
+
+    rotate(nums);
+
+    for (int a : nums) {
+      System.out.println(a);
+    }
+  }
+}
