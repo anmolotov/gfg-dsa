b9d183748e9060234388768a5bc44fc01b414a1f
Anmol Singh <anmolotov05@gmail.com>
2026-01-16 20:05:20 +0530

    Add rotateBy function

--- array/LeftRotate.java
+++ array/LeftRotate.java
@@ -9,12 +9,31 @@ public class LeftRotate {
     arr[arr.length - 1] = temp;
   }
 
+  static void rotateBy(int[] arr, int r) {
+    int[] temp = new int[r];
+
+    for (int i = 0; i < r; i++) {
+      temp[i] = arr[i];
+    }
+
+    for (int i = r; i < arr.length; i++) {
+      arr[i - r] = arr[i];
+    }
+
+    for (int i = 0; i < r; i++) {
+      arr[arr.length - r + i] = temp[i];
+    }
+  }
+
   public static void main(String[] args) {
     int[] nums = { 1, 2, 3, 4, 5 };
+    int[] nums1 = { 1, 2, 3, 4, 5 };
 
     rotate(nums);
 
-    for (int a : nums) {
+    rotateBy(nums1, 2);
+
+    for (int a : nums1) {
       System.out.println(a);
     }
   }
